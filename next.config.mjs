a314059dafec42ced9a5f453321533498c0b2b0f
sqlainsaad5 <sqlainsaad5@gmail.com>
2025-09-29 03:11:23 +0500

    update portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
+// next.config.js for local dev
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  output: 'export',   // ye ok hai
+  // basePath: '/portfolio',   // comment out karo local ke liye
+  // assetPrefix: '/portfolio/', // comment out karo local ke liye
 }
 
 export default nextConfig
